5291b2102b078d4fc1aced8ffe2b45fe4f39bc26
mai-reaska <2820.mai.reaksa@rupp.edu.kh>
2024-10-09 09:59:46 +0700

    clead project architecture

--- src/components/SubNav.jsx
+++
@@ -1,8 +0,0 @@
-function Subnav(){
-    return(
-        <div>
-            <h2>Subnav</h2>
-        </div>
-    )
-}
-export default Subnav;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "../page/Header.jsx";
-import Home from "../Home.jsx";
-import Even from "../components/Even.jsx";
-import Blogs from "../components/SubNav.jsx";
+
 function Router() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/even" element={<Even />} />
-          <Route path="/header" element={<Header />} />
-          <Route path="/blogs" element={<Blogs />} />
         </Routes>
       </BrowserRouter>
     </div>
